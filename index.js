6f39c41fd90e8165e710ed8c869ccc1316b1287c
Akhil Antony K <antonykakhil@gmail.com>
2018-12-12 19:41:22 +0530

    verify jwt

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const jwt = require('njwt');
 
 app.get('/create', (req, res) => {
   const authToken = req.headers.authorization;
-  console.log('req.headers.authorization', req.headers.authorization);
   if (authToken !== 'Basic QXp1cmVEaWFtb25kOmh1bnRlcjI=') {
     res.set('WWW-Authenticate', 'Basic realm="401"');
     res.status(401).send('Try -> username: AzureDiamond, password: hunter2');
@@ -17,13 +16,22 @@ app.get('/create', (req, res) => {
   }
   // res.send(`TODO: verify this JWT ${JSON.stringify(req.headers.authorization)}`);
   const claims = { iss: 'fun', sub: 'AzureDiamond' };
-  const token = jwt.create(claims, 'top-secret-service');
-  token.setExpiration((new Date).getTime() + 1 * 1000);
+  const token = jwt.create(claims, 'top-secret-phrase-aka-secret-key');
+  token.setExpiration((new Date).getTime() + 25 * 1000);
   res.send(token.compact());
 });
 
 app.get('/verify/:token', (req, res) => {
-  res.send(`TODO: verify this JWT token, ${req.params.token}`);
+  const { token } = req.params;
+  jwt.verify(token, 'top-secret-phrase-aka-secret-key',
+    (err, verifiedJwt) => {
+      if (err) {
+        res.send(err.message);
+      }
+      else {
+        res.send(verifiedJwt);
+      }
+    })
 });
 
 app.get('/', (req, res) => res.send('TODO: use OKTA for authorization'));
